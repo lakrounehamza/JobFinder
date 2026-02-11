049ca693456abc87bea4358f28e483886ef6a314
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-11 10:12:10 +0100

    feat : add service for authentification

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {LoginRequest, LoginResponse, RegisterRequest, UserProfile} from '../models/auth.model';
+import {Observable} from 'rxjs';
+import {UserService} from './user';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {
-  
+export class AuthSerivce {
+
+  constructor(private http: HttpClient,private  userService:UserService) {
+  }
+
+  register(request: RegisterRequest) {
+    return this.http.post<UserProfile>("http://localhost:3000/users", request);
+  }
+
+  login(resquest : LoginRequest){
+    this.userService.getAllUser().subscribe(
+      {
+        next: (res:UserProfile[])=>{
+          res.forEach(user=>{
+            if(user.email==resquest.email && user.password==resquest.password)
+              localStorage.setItem("user-auth",JSON.stringify(user));
+          });
+    }
+      }
+    )
+  }
 }
