f929ec7d6f20940f5cdbea479af718c47722720b
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-11 12:06:59 +0100

    feat : add tmplet html   for component register

--- src/app/features/auth/pages/register/register.ts
+++ src/app/features/auth/pages/register/register.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {validate} from '@angular/forms/signals';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
 
+  registerForm :FormGroup;
+  onSubmit(){
+
+  }
+  constructor(private buildForm : FormBuilder) {
+    this.registerForm =this.buildForm.group({
+      nom :['',[Validators.required,Validators.min(3)]],
+      prenom :['',[Validators.required,Validators.min(3)]],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.min(8)]]
+    })
+  }
 }
