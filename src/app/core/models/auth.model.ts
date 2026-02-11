a2939ee0d0d1cc9238de9aa251f834e9031d747c
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-11 10:17:20 +0100

    feat : add   reguest et   response  for register

--- src/app/core/models/auth.model.ts
+++ src/app/core/models/auth.model.ts
@@ -5,3 +5,17 @@ export interface LoginRequest{
 export  interface LoginResponse{
 
 }
+export interface RegisterRequest{
+  nom:string;
+  prenom : string;
+  email:string;
+  password : string;
+}
+
+export interface UserProfile{
+  id:number;
+  nom:string;
+  prenom : string;
+  email:string;
+  password : string;
+}
--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {AuthSerivce} from '../../../../core/services/auth';
+import {LoginRequest, UserProfile} from '../../../../core/models/auth.model';
+import {UserService} from '../../../../core/services/user';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './login.css',
 })
 export class Login {
+  constructor(private loginService: AuthSerivce,private userService :UserService) {
+    this.doLogin();
+  }
+
+  doLogin() {
+    const request: LoginRequest = {
+      email: "email.com",
+      password: "password1"
+    };
+
+    this.loginService.login(request);
+  }
 }
