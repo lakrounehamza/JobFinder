6c0861580e8b89829abe0e38462103c532dcf9e1
lakrounehamza <lakrounehamza10@gmail.com>
2026-02-13 14:03:15 +0100

    fix: add lazy loading  to display jobs

--- src/app/features/home/components/job-list/job-list.ts
+++ src/app/features/home/components/job-list/job-list.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {JobService} from '../../../../core/services/job';
-import {JobResponse} from '../../../../core/models/job.model';
-import {CommonModule} from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { JobService } from '../../../../core/services/job';
+import { JobResponse } from '../../../../core/models/job.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-job-list',
@@ -10,18 +10,46 @@ import {CommonModule} from '@angular/common';
   styleUrl: './job-list.css',
 })
 export class JobList implements OnInit {
-  jobs: JobResponse[] = [];
+  @Input() jobs: JobResponse[] = [];
+  isLoading: boolean = false;
+  errorMessage: string = '';
 
-  constructor(private jopService: JobService) {}
+  constructor(private jobService: JobService) {
+  }
 
   ngOnInit(): void {
-    this.jopService.getAllJob().subscribe({
+    if (this.jobs && this.jobs.length > 0) {
+      console.log('Jobs reçus via @Input:', this.jobs.length);
+      return;
+    }
+
+    this.loadJobsFromService();
+  }
+
+  private loadJobsFromService(): void {
+    const cachedJobs = localStorage.getItem('jobsList');
+    if (cachedJobs) {
+      try {
+        this.jobs = JSON.parse(cachedJobs);
+        console.log('Chargé depuis localStorage:', this.jobs.length);
+      } catch (e) {
+        console.error('Erreur de parsing localStorage');
+      }
+    }
+    this.isLoading = true;
+    this.jobService.getAllJob().subscribe({
       next: (res: any) => {
-        this.jobs = res.data;
-        console.log(this.jobs);
+        if (res && res.data) {
+          this.jobs = res.data;
+          localStorage.setItem('jobsList', JSON.stringify(this.jobs));
+          console.log('Chargé depuis API:', this.jobs.length);
+        }
+        this.isLoading = false;
       },
       error: (err) => {
-        console.error(err);
+        console.error('Erreur:', err);
+        this.errorMessage = 'Erreur lors du chargement des données';
+        this.isLoading = false;
       }
     });
   }
